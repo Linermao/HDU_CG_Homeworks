c3f437b253ea08ad5f4356f796da604654b84be1
Linermao <LinermaoGemail@gmail.com>
2024-05-31 12:17:56 +0800

    5.31 - Final work

---
+++ Final/scripts/light/directional.py
@@ -0,0 +1,8 @@
+from scripts.light.light import Light
+
+
+class DirectionalLight(Light):
+    def __init__(self, color=(1, 1, 1), direction=(0, -1, 0)):
+        super().__init__(Light.DIRECTIONAL)
+        self._color = color
+        self.set_direction(direction)
